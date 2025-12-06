8843168d49af1e4fd282154404f89c082b953395
Rajnish Sharma <srajnish714@gmail.com>
2025-12-07 03:23:55 +0530

    add admin remove admin api added

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -36,7 +36,7 @@ export const createGroup=catchAsync(async (req, res, next) =>{
     if(!myId ) return next(new AppError("userId is required", 400));
     if (!groupName) return next(new AppError("Group name is required", 400));
   
-   const finalMembers = Array.isArray(members) ? members : [];
+   const finalMembers = Array.isArray(members) ? members : [members];
    const newGroup= await Chat.create({
       isGroup:true,
       createdBy:myId,
@@ -67,7 +67,7 @@ export const getMyGroup=catchAsync(async (req, res, next) =>{
 
    if(!groups) return next(new AppError("groups not found", 404));
   
-   res.json({ 
+   res.status(200).json({ 
    message: "Groups fetched successfully",
    data: groups
    })
@@ -90,19 +90,95 @@ export const addGroupMember=catchAsync(async (req, res, next) =>{
     if(!group.isGroup) return next(new AppError("Cannot add members to a 1v1 chat", 400));
     if(!group.admins.some(id => id.toString() === myId)) return next(new AppError("Only admins can add members", 403));
        
-    const finalMembers = Array.isArray(members) ? members : [];
+    const finalMembers = Array.isArray(members) ? members : [members];
   
     const newMember=await Chat.findByIdAndUpdate(chatId, {
     $addToSet: { members: {$each: finalMembers} }},{new: true}
   ).populate("members", "username profilePic").populate("admins", "username")
 
-    res.status(201).json({message:"members added successfully",data:newMember})
+    res.status(200).json({message:"members added successfully",data:newMember})
     io.to(chatId).emit("memberAdded", {chatId, member: newMember});
    })
 
 //--------------Remove group member---------------------------------
 
-export const removeGroupMember=catchAsync(async (req, res, next) =>{
+export const removeGroupMember = catchAsync(async (req, res, next) => {
+
+    const { chatId } = req.query;
+    const myId = req.user.userId;
+    const { members } = req.body;
+
+    if (!myId) return next(new AppError("userId is required", 400));
+    if (!chatId || !members) return next(new AppError("chatId and members are required", 400));
+
+    const group = await Chat.findById(chatId);
+    if (!group) return next(new AppError("Group not found", 404));
+
+    if (!group.isGroup) return next(new AppError("Cannot remove members from a 1v1 chat", 400));
+
+    const isOwner = group.createdBy.toString() === myId;
+    const isAdmin = group.admins.map(a => a.toString()).includes(myId);
+
+    if (!isOwner && !isAdmin) return next(new AppError("Only owner or admins can remove members", 403));
+
+    const finalMembers = Array.isArray(members) ? members : [members];
+
+    const existingMembers = finalMembers.filter(memberId =>
+      group.members.map(m => m.toString()).includes(memberId)
+    );
+
+    if (existingMembers.length === 0) return next(new AppError("User(s) not part of the group", 400));
+
+    const ownerId = group.createdBy.toString();
+    const adminIds = group.admins.map(a => a.toString());
+
+    let removableMembers;
+
+    if (isOwner) {
+      if (existingMembers.includes(myId)) return next(new AppError("Owner cannot remove themselves. Transfer ownership first.", 400));
+      
+      removableMembers = existingMembers;
+    } else {
+      removableMembers = existingMembers.filter(id =>
+        id !== ownerId &&
+        !adminIds.includes(id) &&
+        id !== myId
+      );
+
+      if (removableMembers.length === 0)
+        return next(new AppError("Admins cannot remove owner, other admins, or themselves", 403));
+    }
+  
+    await Chat.findByIdAndUpdate(chatId, {
+      $pull: { members: { $in: removableMembers } }
+    });
+
+    if (isOwner) {
+      await Chat.findByIdAndUpdate(chatId, {
+        $pull: { admins: { $in: removableMembers } }
+      });
+    }
+
+    const updatedGroup = await Chat.findById(chatId)
+      .populate("members", "username profilePic")
+      .populate("admins", "username");
+
+    res.status(200).json({
+      message: "Members removed successfully",
+      removed: removableMembers,
+      data: updatedGroup
+    });
+
+    io.to(chatId).emit("memberRemoved", {
+      chatId,
+      removedMembers: removableMembers
+    });
+
+  });
+
+   //--------------add group admin---------------------------------
+
+export const addGroupAdmin=catchAsync(async (req, res, next) =>{
     
     const { chatId } = req.query;
     const myId = req.user.userId;
@@ -114,23 +190,74 @@ export const removeGroupMember=catchAsync(async (req, res, next) =>{
     const group = await Chat.findById(chatId)
     if(!group) return next(new AppError("group not found", 404));
     
-    if(!group.isGroup) return next(new AppError("Cannot add members to a 1v1 chat", 400));
-    if(!group.admins.some(id => id.toString() === myId)) return next(new AppError("Only admins can removes members", 403));
-       
-    const finalMembers = Array.isArray(members) ? members : [];
-
+    if(!group.isGroup) return next(new AppError("Cannot add admins to a 1v1 chat", 400));
+    if (group.createdBy.toString() !== myId) return next(new AppError("Only the group owner can add admins", 403));
+         
+    const finalMembers = Array.isArray(members) ? members : [members];
+    
     const existingMembers = finalMembers.filter(memberId =>
     group.members.map(m => m.toString()).includes(memberId)
-     );
+  );
 
-    if (existingMembers.length === 0) {
-    return next(new AppError("None of the provided members exist in this group", 400));
-     }
-  
+    if(existingMembers.length === 0)return next(new AppError("User must be a group member to become admin", 400));
+
+  const nonAdminMembers = existingMembers.filter(memberId =>
+    !group.admins.map(a => a.toString()).includes(memberId)
+  );
+
+  if(nonAdminMembers.length === 0) return next(new AppError("User is already an admin", 400));
+   
     const updatedGroup = await Chat.findByIdAndUpdate(chatId, {
-    $pull: { members: {$in: finalMembers} }},
+    $addToSet: { admins: {$each: nonAdminMembers} }},
     {new: true}).populate("members", "username profilePic").populate("admins", "username");
 
-    res.status(201).json({message:"members removed successfully",data:updatedGroup})
-    io.to(chatId).emit("memberRemoved", {chatId, removed: finalMembers});
+    res.status(200).json({message:"admin added successfully",data:updatedGroup})
+    io.to(chatId).emit("adminAdded", {chatId, addedAdmins: nonAdminMembers});
+   })
+
+
+   
+   //--------------remove group admin---------------------------------
+
+export const removeGroupAdmin=catchAsync(async (req, res, next) =>{
+    
+    const { chatId } = req.query;
+    const myId = req.user.userId;
+    const {  members } = req.body;
+  
+    if(!myId ) return next(new AppError("userId is required", 400))
+    if(!chatId || !members) return next(new AppError("chatId and members is required", 400));
+ 
+    const group = await Chat.findById(chatId)
+    if(!group) return next(new AppError("group not found", 404));
+    
+    if(!group.isGroup) return next(new AppError("Cannot remove admins to a 1v1 chat", 400));
+    if (group.createdBy.toString() !== myId) return next(new AppError("Only the group owner can removes admins", 403));
+         
+    const finalMembers = Array.isArray(members) ? members : [members];
+    
+    const existingMembers = finalMembers.filter(memberId =>
+    group.members.map(m => m.toString()).includes(memberId)
+  );
+
+    if(existingMembers.length === 0)return next(new AppError("User must be a group member to remove admin", 400));
+
+  const adminMembers = existingMembers.filter(memberId =>
+    group.admins.map(a => a.toString()).includes(memberId)
+  );
+
+  if(adminMembers.length === 0) return next(new AppError("User is not an admin", 400));
+
+  const ownerId = group.createdBy.toString();
+  const removableAdmins = adminMembers.filter(id => id !== ownerId);
+
+  if (removableAdmins.length === 0)
+    return next(new AppError("Owner cannot be removed as admin", 400));
+  
+    const updatedGroup = await Chat.findByIdAndUpdate(chatId, {
+    $pull: { admins: {$in: removableAdmins} }},
+    {new: true}).populate("members", "username profilePic").populate("admins", "username");
+
+    res.status(200).json({message:"admin removed successfully",data:updatedGroup})
+    io.to(chatId).emit("adminRemoved", {chatId, removedAdmins: removableAdmins});
    })
--- src/routes/chat/chat.router.js
+++ src/routes/chat/chat.router.js
@@ -1,13 +1,17 @@
 import express from "express"
-import { getorCreatePrivateChatId,createGroup, addGroupMember,getMyGroup,removeGroupMember } from "../../controllers/chat.controller.js"
+import { getorCreatePrivateChatId,createGroup, addGroupMember,getMyGroup,removeGroupMember,addGroupAdmin,removeGroupAdmin  } from "../../controllers/chat.controller.js"
 import { verifyAccessToken } from "../../midlleware/auth.middleware.js"
 
 const chatRouter = express.Router()
 
 chatRouter.post("/private",verifyAccessToken,getorCreatePrivateChatId)
 chatRouter.post("/create-group",verifyAccessToken,createGroup)
-chatRouter.patch("/add-member/",verifyAccessToken,addGroupMember)
-chatRouter.delete("/remove-member/",verifyAccessToken,removeGroupMember)
+
+chatRouter.patch("/add-member",verifyAccessToken,addGroupMember)
+chatRouter.patch("/add-admin",verifyAccessToken,addGroupAdmin)
+
+chatRouter.delete("/remove-admin",verifyAccessToken,removeGroupAdmin)
+chatRouter.delete("/remove-member",verifyAccessToken,removeGroupMember)
 
 chatRouter.get("/get-groups",verifyAccessToken,getMyGroup)
 
